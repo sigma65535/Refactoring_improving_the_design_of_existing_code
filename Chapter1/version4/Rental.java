e6d53a70fcad2f6a1a37287846bd502633a03eb8
sigma65535 <362416593@qq.com>
2016-09-21 22:37:11 +0800

    Add files via upload

---
+++ Chapter1/version4/Rental.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @author sigma
+
+ */
+package Chapter1.version4;
+
+class Rental {
+
+	Movie _movie;
+	private int _daysRented;
+	private Price _price;
+
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+
+	public int getDaysRented() {
+		return _daysRented;
+	}
+	
+	public Movie getMovie() {
+		return _movie;
+	}
+	
+	int getFrequentRenterPoints() {
+		return _movie.getFrequentRenterPoints(_daysRented);
+	};
+	
+	double getCharge(){
+		return _price.getCharge(_daysRented);
+	}
+}
